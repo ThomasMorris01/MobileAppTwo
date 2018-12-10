97ee372e6b4cfe8126b6cf9b3fd059b5c6b71b94
Tom Morris <467270@chrome.sussexdowns.ac.uk>
2018-12-10 13:59:16 +0000

    Game Update 1 - Map and Character Movement

---
+++ Assets/Scripts/UpAndDownTwo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class UpAndDownTwo : MonoBehaviour
+{
+    public AnimationCurve myCurve;
+   
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }
+}
+ 
