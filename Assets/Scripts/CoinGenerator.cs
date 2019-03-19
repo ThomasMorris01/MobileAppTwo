5cbbc3ae8819cc6b475da0e8830fe1b03855123b
ThomasMorris01 <467270@chrome.sussexdowns.ac.uk>
2019-03-19 13:50:20 +0000

    Test

---
+++ Assets/Scripts/CoinGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        for (int i = 0; i < 6; i++)
+            Instantiate(prefab, new Vector3(0, 0, i* 1.0f), Quaternion.identity);
+    }
+   
+}
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,18 +4,21 @@ using UnityEngine.UI;
 public class PlayerCollision : MonoBehaviour {
 
     public PlayerMovement movement;
+    
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Restart")
         {
-            movement.enabled = false;          
+            movement.enabled = false;
+            ScoreScript.scoreValue = 0;
             FindObjectOfType<GameManager>().EndGame();
         }
 
         if (collisionInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            ScoreScript.scoreValue = 0;
             FindObjectOfType<GameManager>().EndGame();
             
         }
